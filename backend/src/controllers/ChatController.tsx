7934e1256630652586a525f153bc55f8440116cd
devkrsna <krishnagavali973@gmail.com>
2026-01-28 21:31:33 +0530

    refactor: optimize ChatController stream handling and adjust AppSidebar padding

--- backend/src/controllers/ChatController.tsx
+++ backend/src/controllers/ChatController.tsx
@@ -2,22 +2,18 @@ import type { Request, Response } from "express";
 import { streamText } from "ai";
 import { ollama } from "ai-sdk-ollama";
 import { convertToModelMessages } from "ai";
-import { Readable } from "node:stream";
 
 const ChatController = async (req: Request, res: Response) => {
   const { messages } = req.body;
 
-  const result = await streamText({
+  const result = streamText({
     model: ollama("ministral-3:3b"),
     messages: await convertToModelMessages(messages),
+    tools: {
+      web_search: ollama.tools.webSearch({}),
+    },
   });
-
-  const stream = result.textStream;
-
-  for await (const chunk of stream) {
-    console.log(chunk);
-    res.write(chunk);
-  }
+  result.pipeUIMessageStreamToResponse(res);
 };
 
 export default ChatController;
