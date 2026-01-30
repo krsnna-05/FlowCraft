3310f56e97df94db8cb8f2e8e42415db3af9a624
devkrsna <krishnagavali973@gmail.com>
2026-01-30 20:14:11 +0530

    feat: update system prompts structure and integrate intent handling in ChatController

--- backend/src/controllers/ChatController.tsx
+++ backend/src/controllers/ChatController.tsx
@@ -2,6 +2,7 @@ import type { Request, Response } from "express";
 import { streamText } from "ai";
 import { ollama } from "ai-sdk-ollama";
 import { convertToModelMessages } from "ai";
+import systemPrompts from "../systemPrompts/system.json" with { type: "json" };
 
 const ChatController = async (req: Request, res: Response) => {
   const { messages } = req.body;
@@ -9,6 +10,7 @@ const ChatController = async (req: Request, res: Response) => {
   const result = streamText({
     model: ollama("ministral-3:3b"),
     messages: await convertToModelMessages(messages),
+    system: systemPrompts.intent.content,
     tools: {
       web_search: ollama.tools.webSearch({}),
     },
